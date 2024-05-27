337fdf42ca831acf52139a1c31c40c477399d7e3
Student <Arsenii_proStoFile_007@gmail.com>
2024-05-27 13:58:43 +0300

    UNIT_1

--- keyboard_helper.py
+++ keyboard_helper.py
@@ -1,10 +1,15 @@
 from aiogram.types import ReplyKeyboardMarkup
 from aiogram.types import KeyboardButton
-
+from userstates import UserStates
 
 def get_keyboard(buttons):
     kb = []
-    from button in buttons:
+    for button in buttons:
         kb.append([KeyboardButton(text=button)])
     reply_markup = ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
     return reply_markup
+
+keyboards = {
+    UserStates.BASE: get_keyboard(["Мои пари", "Создать пари"]),
+    UserStates.CREATING_PARI: get_keyboard(["Отмена"])
+}
--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 import asyncio
-from config import TOKEN
 
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.filters import StateFilter
+from config import TOKEN
+from userstates import UserStates
+from keyboard_helper import keyboards
+
+storage = MemoryStorage()
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
 @dp.message(Command("start"))
-async def send_welcome(message: types.Message):
-    await bot.send_message(message.chat.id, "Привет! Я эхо-лот, ха-ха-ха")
+async def send_welcome(message: types.Message, state: FSMContext):
+    kb = keyboards[UserStates.BASE]
+    await message.answer("Привет! Я эхо-лот, ха-ха-ха", reply_markup=kb)
+    await state.set_state(UserStates.BASE)
 
+@dp.message(Command("test"), StateFilter(UserStates.BASE))
+async def start(message: types.Message):
+    await message.answer("О нет! Что ты наделал?! Теперь мы все умреееем!")
 
 async def main():
     await dp.start_polling(bot)
